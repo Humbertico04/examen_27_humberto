6d8bb45918dcb0efd17ae2cc7e8f2ec8218bfb35
Humbertico04 <humbertico2004@gmail.com>
2022-10-27 11:08:16 +0200

    Ejercicio5.py

---
+++ descomposicion.py
@@ -0,0 +1,21 @@
+import sys
+
+def pedir_numero():
+    while True:
+        try:
+            numero= int(input("Introduce un número entero: "))
+        except:
+            sys.stderr, print("Solo están autorizados los caracteres 0-9")
+        else:
+            return str(numero)
+
+def descomposicion(numero):
+    contador = 0
+    while True:
+        for digito in numero:
+            contador+=1
+            print((digito.ljust(int(contador), '0')).zfill(len(numero)), "\n")
+        if contador >= len(numero):
+            break
+
+descomposicion(pedir_numero()[::-1])
